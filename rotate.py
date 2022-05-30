0b84504134fac99c3e473b5fcb6aba74f63e560a
Ann <adea820616@gmail.com>
2022-05-31 04:19:29 +0800

    [clean]remove the unused code

--- rotate.py
+++ rotate.py
@@ -15,11 +15,13 @@ def rotate(array, angle_in_degrees):
     # Find the centre of the image
     original_centre_height = round(h/2)
     original_centre_width = round(w/2)
+
+    # Find the centre of the new image
+    new_centre_height = round(new_height/2)
+    new_centre_width = round(new_width/2)
     
     for x,y in itertools.product(range(w), range(h)):
-        pixels = array[y,x,:]
-        new_x, new_y = np.matmul(rotation_matrix, [x,y])
-        new_x, new_y = int(new_x), int(new_y)
+        
         if 0 <= new_x < w and 0 <= new_y < h:
             result[new_y, new_x, :] = pixels
             
